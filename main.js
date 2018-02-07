24c8b21ff3db31e233d6006ee184b443a287e067
chriskmaddin <chriskmaddin@gmail.com>
2018-02-07 10:49:03 -0600

    working on user coffee query feature. still working out bugs with return values.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 function renderCoffee(coffee) {
     var html = '<tr class="coffee">';
@@ -12,7 +12,7 @@ function renderCoffee(coffee) {
 
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    for(var i = 0; i < coffees.length; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
@@ -29,6 +29,25 @@ function updateCoffees(e) {
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
+function searches() {
+    var coffeeType = coffeeValues.value;
+    var searchedCoffees = [];
+    coffees.forEach(function(coffee)
+    {
+        if (coffee.name === coffeeType) {
+            searchedCoffees.push(coffee);
+
+        } if (searchedCoffees.length === 0) {
+        tbody.innerHTML = "That is not a coffee we have";
+    }
+        // else {
+        // }
+    });
+    tbody.innerHTML = renderCoffee(searchedCoffees);
+
+    console.log(searchedCoffees);
+}
+
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
@@ -45,13 +64,16 @@ var coffees = [
     {id: 11, name: 'Espresso', roast: 'dark'},
     {id: 12, name: 'Viennese', roast: 'dark'},
     {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'}
 ];
 
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
-
+var coffeeValues = document.getElementById("coffee-input");
+var searchSubmit = document.getElementById("search-result");
 tbody.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
+searchSubmit.addEventListener('click', searches);
+
